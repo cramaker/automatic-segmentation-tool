e3ca4c36d1b6e8b269b197f55a9a9df064efa66d
Cody Ramaker <cramaker@linux.com>
2020-01-08 16:21:41 -0800

    main.py simplifications

--- main.py
+++ main.py
@@ -16,14 +16,6 @@ if __name__ == "__main__":
     base = "/home/cramaker/Documents/master_thesis/test_data/"
 
     fileset = check_files(base)
-    # fileset = {"9gag.com.html", "agh.edu.pl.html", "brw.pl.html", "caranddriver.com.html",
-    #            "craigslist.com.html", "disney.pl.html", "dobreprogramy.pl.html"}
-    # fileset = {"index.html"}
-    segmented_simple = []
-
-    segmented_fuzzy = []
-
-
     for i in fileset:
 
         files = ["a." + i]
@@ -42,15 +34,3 @@ if __name__ == "__main__":
         # visualize
         for i in range(0, len(all_segs)):
             segmentation.visualize(all_segs[i], "/home/cramaker/Documents/" + files[i])
-
-        # load reference pages and measure
-    #     if len(all_segs[0]) > 0:
-    #         for ii in range(0, len(all_segs)):
-    #             simpl = segmentation.simple_measure(all_segs[ii][0], files[ii])
-    #             fuzz = segmentation.fuzzy_measure(all_segs[ii][0], files[ii])
-    #             print files[ii], simpl, fuzz
-    #             segmented_simple.append(simpl)
-    #             segmented_fuzzy.append(fuzz)
-
-    # print segmentation.comulative(segmented_simple)
-    # print segmentation.comulative_fuzzy(segmented_fuzzy)
